f2029c8c55213e4fef561c198684565036684d26
Peter Horvath <horvthpeter@gmail.com>
2025-02-12 21:43:32 +0200

    Fix problem 4

--- array_prod_except_self_238.py
+++ array_prod_except_self_238.py
@@ -1,9 +1,22 @@
 from typing import List
-from operator import mul
-from functools import reduce
 
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prod = reduce(mul, nums)
-        return [prod / x for x in nums]
+        nr_zeros = 0
+        for num in nums:
+            if num == 0:
+                nr_zeros += 1
+
+        if nr_zeros > 1:
+            return [0] * len(nums)
+
+        product = 1
+        for num in nums:
+            if num != 0:
+                product *= num
+
+        if nr_zeros == 1:
+            return [0 if x != 0 else product for x in nums]
+
+        return [product / x for x in nums]
