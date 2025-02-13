ecc66fb6196b29512d12ee8226101f66fe6d7c40
Peter Horvath <horvthpeter@gmail.com>
2025-02-13 09:58:51 +0200

    Problem 4: Fix logic

--- array_prod_except_self_238.py
+++ array_prod_except_self_238.py
@@ -12,11 +12,26 @@ class Solution:
             return [0] * len(nums)
 
         product = 1
+        prefix_products = []
         for num in nums:
-            if num != 0:
-                product *= num
+            product *= num
+            prefix_products.append(product)
 
-        if nr_zeros == 1:
-            return [0 if x != 0 else product for x in nums]
+        product = 1
+        suffix_products = [None] * len(nums)
+        for i in range(len(nums) - 1, -1, -1):
+            product *= nums[i]
+            suffix_products[i] = product
 
-        return [product // x for x in nums]
+        res = []
+        for i, num in enumerate(nums):
+            left = prefix_products[i-1] if i > 0 else 1
+            right = suffix_products[i+1] if i < len(nums) - 1 else 1
+            if i == 0:
+                left = 1
+            if i == len(nums) - 1:
+                right = 1
+            product = left * right
+            res.append(product)
+
+        return res
